d50968ae6263da886ebb72e17763a15e24f8e88c
Romaniv1912 <romaniv1912@gmail.com>
2024-12-18 17:50:57 +0200

    add logger

--- src/common/settings.py
+++ src/common/settings.py
@@ -4,9 +4,10 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 class AppSettings(BaseSettings):
     """App Settings"""
 
-    model_config = SettingsConfigDict(env_prefix='APP_')
+    model_config = SettingsConfigDict(env_prefix="APP_")
 
     # FastAPI
+    DEBUG: bool = False
     TITLE: str
     VERSION: str
     DESCRIPTION: str
@@ -15,4 +16,36 @@ class AppSettings(BaseSettings):
     OPENAPI_URL: str | None = None
     BASE_PATH: str
 
+    # Database
     DB_URL: str
+    DB_ECHO: bool = False
+
+    # Trace ID
+    TRACE_ID_REQUEST_HEADER_KEY: str = "X-Request-ID"
+
+    # CORS Setting
+    MIDDLEWARE_CORS: bool = True
+    CORS_ALLOWED_ORIGINS: tuple[str] = ("*",)
+    CORS_EXPOSE_HEADERS: tuple[str] = (TRACE_ID_REQUEST_HEADER_KEY,)
+
+
+class LogSettings(BaseSettings):
+    """Logs Settings"""
+
+    model_config = SettingsConfigDict(env_prefix="LOG_")
+
+    ROOT_LEVEL: str = "NOTSET"
+    STD_FORMAT: str = (
+        "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</> | <lvl>{level: <8}</> | "
+        "<cyan> {correlation_id} </> | <lvl>{message}</>"
+    )
+    LOGURU_FORMAT: str = (
+        "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</> | <lvl>{level: <8}</> | "
+        "<cyan> {correlation_id} </> | <lvl>{message}</>"
+    )
+    CID_DEFAULT_VALUE: str = "-"
+    CID_UUID_LENGTH: int = 32  # must <= 32
+    STDOUT_LEVEL: str = "INFO"
+    STDERR_LEVEL: str = "ERROR"
+    STDOUT_FILENAME: str = "api_access.log"
+    STDERR_FILENAME: str = "api_error.log"
--- src/core/conf.py
+++ src/core/conf.py
@@ -1,27 +1,24 @@
 import os
 from functools import lru_cache
 from pathlib import Path
-from typing import Literal
 
 from pydantic_settings import BaseSettings
 
-from src.common.settings import AppSettings, DatabaseSettings
+from src.common.settings import AppSettings, LogSettings
 
 # Get the project root directory
 # Or use an absolute path to the backend directory
 BASE_PATH = Path(__file__).resolve().parent.parent.parent
 
 # Log file path
-LOG_DIR = os.path.join(BASE_PATH, 'logs')
+LOG_DIR = os.path.join(BASE_PATH, "logs")
 
 
 class Settings(BaseSettings):
     """Global Settings"""
 
-    # Env Config
-    ENVIRONMENT: Literal['dev', 'pro']
-
     APP: AppSettings = AppSettings()
+    LOG: LogSettings = LogSettings()
 
 
 @lru_cache
