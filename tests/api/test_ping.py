bfbe00e59724cb31f0643a79b8b83e183dd4cd54
Romaniv1912 <romaniv1912@gmail.com>
2024-12-18 22:51:01 +0200

    add ping route test

---
+++ tests/api/test_ping.py
@@ -0,0 +1,8 @@
+from starlette.testclient import TestClient
+
+
+def test_ping(client: TestClient) -> None:
+    resp = client.get("/ping")
+
+    assert resp.status_code == 200, "Bad status code"
+    assert resp.json() == {"ping": "pong"}, "Response is not a pong response"
