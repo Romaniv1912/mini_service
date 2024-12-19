a36fd0566bd0001f5818c4f8e2b10874dff83124
Romaniv1912 <romaniv1912@gmail.com>
2024-12-19 12:17:44 +0200

    add new pagination type

--- src/common/schema.py
+++ src/common/schema.py
@@ -1,5 +1,46 @@
+import math
+
+from typing import Generic, List, Tuple, TypeVar
+
+from fastapi import Query
 from pydantic import BaseModel, ConfigDict
 
 
 class SchemaBase(BaseModel):
     model_config = ConfigDict(use_enum_values=True)
+
+
+TSchema = TypeVar('TSchema')
+
+
+class GetPageParams(BaseModel):
+    page: int = Query(1, ge=1, description='Page number')
+    size: int = Query(20, gt=0, le=100, description='Page size')  # Default 20 records
+
+    def get_raw(self) -> Tuple[int, int]:
+        return self.size, self.size * (self.page - 1)
+
+
+class GetList(SchemaBase, Generic[TSchema]):
+    items: List[TSchema]  # data
+    total: int  # total number of data
+
+    def __init__(self, items: List[TSchema], **kwargs):
+        total = kwargs.pop('total', len(items))
+        super().__init__(items=items, total=total, **kwargs)
+
+
+class GetListPage(GetList):
+    page: int  # page n
+    size: int  # quantity per page
+    total_pages: int  # total pages
+
+    @classmethod
+    def create(cls, items: List[TSchema], total: int, params: GetPageParams):
+        return cls(
+            items=items,  # type: ignore
+            total=total,  # type: ignore
+            page=params.page,
+            size=params.size,
+            total_pages=math.ceil(total / params.size),
+        )
